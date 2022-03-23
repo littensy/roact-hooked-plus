1f3da4e156e1bfa8e0f9de7bdec81584ff93f297
Richard <56808540+littensy@users.noreply.github.com>
2022-03-23 12:50:01 -0700

    Add exports

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,22 @@ export { asBinding, getBindingValue, isBinding, mapBinding } from "./utils/bindi
 export { Timeout, clearTimeout, setTimeout } from "./utils/set-timeout";
 export { Interval, clearInterval, setInterval } from "./utils/set-interval";
 
+export { useAnimation } from "./use-animation";
+export { useClickOutside } from "./use-click-outside";
+export { useDebouncedValue } from "./use-debounced-value";
+export { useDelayedValue } from "./use-delayed-value";
+export { useDidMount } from "./use-did-mount";
+export { useForceUpdate } from "./use-force-update";
+export { useGroupMotor } from "./use-group-motor";
+export { useHotkeys } from "./use-hotkeys";
+export { useIdle } from "./use-idle";
+export { useInterval } from "./use-interval";
+export { useListState } from "./use-list-state";
+export { useMouse } from "./use-mouse";
+export { usePromise } from "./use-promise";
+export { useSequence } from "./use-sequence";
+export { useSetState } from "./use-set-state";
+export { useSingleMotor } from "./use-single-motor";
+export { useToggle } from "./use-toggle";
 export { useViewportSize } from "./use-viewport-size";
 export { SpringOptions, getBinding, useGoal, useInstant, useLinear, useMotor, useSpring } from "./flipper";
