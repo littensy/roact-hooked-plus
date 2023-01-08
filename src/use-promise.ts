3f9d4e487cb685cd9778ada86a4060bf91c481ba
Richard <richerd.rdc@gmail.com>
2023-01-07 23:14:43 -0800

    Fix status resolution

--- src/use-promise.ts
+++ src/use-promise.ts
@@ -16,17 +16,12 @@ export function usePromise<T>(promise: Promise<T> | (() => Promise<T>), dependen
 			setState({ status: Promise.Status.Started });
 		}
 
-		let result: T | undefined;
-		let errorMessage: unknown;
+		const promiseToUse = typeIs(promise, "function") ? promise() : promise;
 
-		const promiseToUse = (typeIs(promise, "function") ? promise() : promise)
-			.then(
-				(promiseResult) => (result = promiseResult),
-				(promiseErrorMessage: unknown) => (errorMessage = promiseErrorMessage),
-			)
-			.done((status) => {
-				setState({ status, result, errorMessage });
-			});
+		promiseToUse.then(
+			(result) => setState({ status: promiseToUse.getStatus(), result, errorMessage }),
+			(errorMessage: unknown) => setState({ status: promiseToUse.getStatus(), result, errorMessage }),
+		);
 
 		return () => {
 			promiseToUse.cancel();
